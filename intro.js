0d9c3aa0bdb32a01592a325711c3759be77df45c
Swapnil Jain <swapnil.jain@iiitb.org>
2021-03-21 18:46:33 +0530

    DOM Added

--- intro.js
+++ intro.js
@@ -29,4 +29,31 @@ var c=a+b
 //FUNCTIONS
 // Function syntax
 // function <function-name> (<formal parameter>) { function definiton  return <any-output you want>;}  function create
-// <function-name>(<actual parameter>);  function call
+//  <function-name>(<actual parameter>);  function call
+
+// to generate random number
+var n=Math.random();// it generate 16 decimal places no. bt 0-.9999999999999999
+
+// to get between range then multiply with that Number
+// ex. you want number bt 0-7 then multiply it with 7 like Math.random();
+
+// Here we use === 3equals to check the equality and !== for unequal
+differnce == it only checks for equality not datatype
+          === it only checks for equality and datatype both
+Arrays
+var myarr=[1,2,3,4,5];
+
+// to find that element is in that array or not we use include
+myarr.includes(2,0);
+// to add element in the array
+myarr.push(6);
+// to remove element in the array
+myarr.pop();
+
+// LOOPS
+while(condition){
+  // do something
+}
+for(initialize;condition;updation){
+  // do something
+}
