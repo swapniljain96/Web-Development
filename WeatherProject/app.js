dc5164b4aa0b6ea56abdbbc42f37244b9888ccfa
Swapnil Jain <swapnil.jain@iiitb.org>
2021-03-24 14:03:20 +0530

    Weather Project Done

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,31 +1,41 @@
 const express = require("express");
 const https = require("https");
+const bodyParser=require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res) {
+app.use(bodyParser.urlencoded({extended:true}));
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=e00c1dc031351ae168b18bad2bd158a5&units=metric";
+// GET FUNCTION
+app.get("/", function(req, res) {
+  res.sendFile(__dirname+"/index.html");
+})
+
+// POST FUNCTION
+app.post("/",function(req, res){
+  //console.log("Post Received");
+  // console.log(req.body.cityName);
+  const city=req.body.cityName;
+  const apiKey="e00c1dc031351ae168b18bad2bd158a5";
+  const unit="metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey+"&units="+unit;
   https.get(url, function(response) {
-    console.log(response.statusCode);
+    // console.log(response.statusCode);
 
     response.on("data", function(data) {
       // console.log(data);  that gives data in HEX format
       const weatherData = JSON.parse(data);
-      console.log(weatherData);
+      // console.log(weatherData);
       const temp = weatherData.main.temp;
-      console.log(temp);
+      // console.log(temp);
       const desc = weatherData.weather[0].description;
-      console.log(desc);
+      // console.log(desc);
       const icon=weatherData.weather[0].icon;
       const imageURL="http://openweathermap.org/img/wn/"+icon+"@2x.png"
-      res.write("<h1>The temperature in London is: " + temp + " degree Celcius</h1>");
-      res.write("<p>The weather in London is: " + temp + " <p>");
+      res.write("<h1>The temperature in "+city+" is: " + temp + " degree Celcius</h1>");
+      res.write("<p>The weather in "+city+" is: " + temp + " <p>");
       res.write("<img src="+imageURL+">");
       res.send();
-
-
-
       // const object={
       //   name:"Swapnil",
       //   clg:"IIIT-B"                            this stringify function changes
@@ -35,8 +45,11 @@ app.get("/", function(req, res) {
     })
   })
   //res.send("Server is up and running"); /// this can't be here because we cant have 2 res.send();
+
 })
 
+
+// LISTEN FUNCTION
 app.listen(3000, function() {
   console.log("Server is running in port 3000.");
 })
