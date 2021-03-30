c61a24eb6b98f1547c5e8d4444fffabdea9825bb
Swapnil Jain <swapnil.jain@iiitb.org>
2021-03-30 19:11:03 +0530

    Keeper App Part 1 Solution Added

---
+++ Keeper App/src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+var date= new Date();
+var year=date.getFullYear();
+function Footer(){
+
+    return (
+        <footer>
+        <p>Copyright ⓒ {year} </p>
+         </footer>
+    )
+}
+
+export default Footer;  
