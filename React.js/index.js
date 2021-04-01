e25e17c5896e9c35b260c01ce3abdf27efd84454
Swapnil Jain <swapnil.jain@iiitb.org>
2021-04-01 09:33:32 +0530

    React Custom Attributes

--- React.js/index.js
+++ React.js/index.js
@@ -110,3 +110,34 @@ function Heading() {
 }
 
 export default Heading;  //export default means any file can access that file
+
+// here we are using React custom made attribute called props 
+// this is passing the arguments to the functions
+function Card(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+ReactDOM.render(
+  <div>
+    <h1>My Contacts</h1>
+    <Card
+      name="Beyonce"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      tel="+123 456 789"
+      email="b@beyonce.com"
+    />
+    <Card
+      name="Jack Bauer"
+      img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+      tel="+987 654 321"
+      email="jack@nowhere.com"
+    />
+  </div>,
+  document.getElementById("root")
+);
